7afab3af440684c6e808fd08fa9277d4e990cbae
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2021-03-24 16:25:22 +0100

    feat: let user choose from a list of source directories

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,29 +1,61 @@
 #!/usr/bin/env node
 
 import fastline, { FastlineOptions } from "@fastline/core";
-import inquirer from "inquirer";
+import { prompt } from "inquirer";
+import path from "path";
+import fs from "fs-extra";
 
-type Answers = Pick<FastlineOptions, "srcDir" | "outDir">;
+type Answers = [
+  {
+    outDir: string;
+    templatesRoot: string;
+  },
+  {
+    srcDir: string;
+  }
+];
 
 async function main() {
-  const answers = await inquirer.prompt<Answers>(
-    [
-      {
-        name: "outDir",
-        message: "Path to the out directory",
-        default: "./out",
-        when: typeof process.argv[2] !== "string",
-      },
-      {
-        name: "srcDir",
-        message: "Path to the source directory",
-      },
-    ],
+  console.log("Fastline CLI");
+  const { outDir, templatesRoot } = await prompt<Answers[0]>([
     {
-      outDir: process.argv[2],
-    }
-  );
-  console.log(answers);
+      type: "input",
+      name: "outDir",
+      message: "Enter the path to your directory",
+      default: `${process.cwd()}/myapp`,
+    },
+    {
+      type: "input",
+      name: "templatesRoot",
+      message:
+        "Choose a template directory or use the default templates (hit ENTER to select default)",
+      default: path.resolve(__dirname, "../templates"),
+    },
+  ]);
+
+  const templateFiles = await fs.readdir(templatesRoot);
+
+  const availableTemplates = templateFiles
+    // Prepend absolute path to the filename
+    .map((fileName) => path.join(templatesRoot, fileName))
+    // Filter directories
+    .filter((filePath) => fs.statSync(filePath).isDirectory());
+
+  const { srcDir } = await prompt<Answers[1]>([
+    {
+      type: "list",
+      name: "srcDir",
+      choices: availableTemplates,
+    },
+  ]);
+
+  await fastline({
+    outDir: outDir,
+    srcDir: srcDir,
+    substitutions: {},
+  });
+
+  console.log("Done");
 }
 
 main();
