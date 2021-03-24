00d7e1187400f6f6e304f1ab64770561eca8bbe2
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2021-03-24 10:40:18 +0100

    feat: add rudimentary command line program

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello world");
+#!/usr/bin/env node
+
+import fastline from "@fastline/core";
+
+fastline({
+  outDir: process.argv[2] || "./out",
+  srcDir: process.argv[3] || "./node_modules/@fastline/hello-world",
+  substitutions: {
+    [process.argv[4] || "name"]: process.argv[5] || "fastline",
+  },
+});
